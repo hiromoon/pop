c4e60a449b8cf252c38032e82b0c2627f6af3ba5
hiromoon <hiromoon0428@gmail.com>
2017-07-23 02:05:24 +0900

    [add] Userテーブル、モデルを追加

---
+++ priv/repo/migrations/20170722162905_user.exs
@@ -0,0 +1,16 @@
+defmodule Pop.Repo.Migrations.User do
+  use Ecto.Migration
+
+  def change do
+    create table(:users) do
+      add :username, :string, unique: true
+      add :encrypted_password, :string, null: false
+      add :email, :string
+      add :confirmed, :boolean, default: false
+
+      timestamps
+    end
+
+    create unique_index(:users, [:username], name: :unique_usernames)
+  end
+end
---
+++ web/models/user.ex
@@ -0,0 +1,43 @@
+defmodule Pop.User do
+  use Pop.Web, :model
+  import Comeonin.Bcrypt, only: [hashpwsalt: 1]
+
+  schema "users" do
+    field :username, :string
+    field :encrypted_password, :string
+    field :email, :string
+    field :confirmed, :boolean, default: false
+    field :password, :string, virtual: true
+    field :password_confirmation, :string, virtual: true
+  end
+
+  @required_fields ~w(username encrypted_password email)
+  @optional_fields ~w()
+
+  def changeset(user, params \\ :empty) do
+    user
+    |> cast(params, @required_fields, @optional_fields)
+    |> validate_length(:password, min: 8)
+    |> validate_password_confirmation()
+    |> unique_constraint(:username)
+    |> put_change(:encrypted_password, hashpwsalt(params[:password]))
+  end
+
+  defp validate_password_confirmation(changeset) do
+    case get_change(changeset, :password_confirmation) do
+      nil ->
+        password_incorrect_error(changeset)
+      confirmation ->
+        password = get_field(changeset, :password)
+        if confirmation == password, do: changeset, else: password_mismatch_error(changeset)
+    end
+  end
+
+  defp password_mismatch_error(changeset) do
+    add_error(changeset, :password_confirmation, "Passwords does not match")
+  end
+
+  defp password_incorrect_error(changeset) do
+    add_error(changeset, :password, "is not valid")
+  end
+end

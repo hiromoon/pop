5fe6cbdd7ed919e5ce6aa7a76907f51df7e364e5
hiromoon <hiromoon0428@gmail.com>
2017-07-23 23:18:21 +0900

    [add] UserのCreateとReadを追加

---
+++ web/controllers/user_controller.ex
@@ -0,0 +1,32 @@
+defmodule Pop.UserController do
+  use Pop.Web, :controller
+  alias Pop.User
+
+  def index(conn, _params) do
+    users = Repo.all(User)
+    render conn, "index.html", users: users
+  end
+
+  def new(conn, _params) do
+    changeset = User.changeset(%User{})
+    render conn, "new.html", changeset: changeset
+  end
+
+  def create(conn, %{"user" => user_params}) do
+    changeset = User.changeset(%User{}, user_params)
+    case Repo.insert(changeset) do
+      {:ok, user} ->
+        conn
+        |> put_flash(:info, "#{user.name}を作成しました。")
+        |> redirect(to: user_path(conn, :index))
+
+      {:error, changeset} ->
+        render(conn, "new.html", changeset: changeset)
+    end
+  end
+
+  def show(conn, %{"id" => id}) do
+    user = Repo.get(User, id)
+    render conn, "show.html", user: user
+  end
+end

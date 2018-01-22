7a9df01c6d7979a8f6f8a033914a49a480171b77
hiromoon <hiromoon0428@gmail.com>
2018-01-22 22:46:58 +0900

    [modify] 必須パラメータのチェックを追加

--- lib/pop_web/controllers/oauth/v2/authorization_controller.ex
+++ lib/pop_web/controllers/oauth/v2/authorization_controller.ex
@@ -1,8 +1,24 @@
 defmodule PopWeb.Authorizationcontroller do
   use PopWeb, :controller
+  import Plug.Conn
 
-  def index(conn, _) do
-    # conn = Plug.Conn.put_session(conn, :pop_auth, "hoge")
-    redirect conn, to: "/signin"
+  def index(conn, params) do
+    conn
+    |> validation_required(params, ["scope", "response_type", "redirect_uri", "client_id"])
+    |> put_resp_cookie("_pop_auth", "hoge")
+    |> redirect to: "/signin"
+  end
+
+  defp validation_required(conn, params, list) do
+    case check_required(params, list) do
+      true -> conn
+      false -> raise "Invalid Request"
+    end
+  end
+
+  defp check_required(params, list) do
+    list
+    |> Enum.map(fn(x) -> Map.has_key?(params, x) end)
+    |> Enum.all?(fn(x) -> x end)
   end
 end

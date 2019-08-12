9c4ce3b80819e0868f72f016a7e5641ff4631ad4
hiromoon <hiromoon0428@gmail.com>
2019-08-12 21:36:04 +0900

    hooksで書き換え

--- assets/src/containers/Signin.js
+++ assets/src/containers/Signin.js
@@ -1,52 +1,27 @@
-import React from 'react'
+import React, {useCallback} from 'react'
 import PropTypes from 'prop-types'
-import { connect } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 import SigninComponent from '../components/Signin'
 import { changeText } from '../actions/Signin'
 
 /** Signin page container **/
-class Signin extends React.Component {
-  /**
-   * @return {React.Element} Signin Page
-   */
-  render() {
-    const {
-      userId,
-      password,
-      changeUserIdHandle,
-      changePasswordHandle,
-    } = this.props
-    return (
-      <SigninComponent
-        userId={userId}
-        password={password}
-        onChangeUserId={changeUserIdHandle}
-        onChangePassword={changePasswordHandle}
-      />
-    )
-  }
+const Signin = () => {
+  const {userId, password} = useSelector(state => state.Signin)
+  const dispatch = useDispatch()
+  const changeUserIdHandle = useCallback((event) =>
+    dispatch(changeText('userId', event.target.value)), [])
+  const changePasswordHandle = useCallback((event) =>
+    dispatch(changeText('password', event.target.value)), [])
+  return (
+    <SigninComponent
+      userId={userId}
+      password={password}
+      onChangeUserId={changeUserIdHandle}
+      onChangePassword={changePasswordHandle}
+    />
+  )
 }
 
-Signin.propTypes = {
-  userId: PropTypes.string.isRequired,
-  password: PropTypes.string.isRequired,
-  changeUserIdHandle: PropTypes.func.isRequired,
-  changePasswordHandle: PropTypes.func.isRequired,
-}
+Signin.propTypes = {}
 
-const mapStateToProps = state => ({
-  userId: state.Signin.userId,
-  password: state.Signin.password,
-})
-
-const mapDispatchToProps = dispatch => ({
-  changeUserIdHandle: event =>
-    dispatch(changeText('userId', event.target.value)),
-  changePasswordHandle: event =>
-    dispatch(changeText('password', event.target.value)),
-})
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(Signin)
+export default Signin
